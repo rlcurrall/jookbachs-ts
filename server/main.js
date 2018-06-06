7124409cb97f453ccaa70f8c2e7d3aaaca76099d
sweylo <nichols.logan@gmail.com>
2018-06-06 04:26:08 -0400

    more trying with electron

--- server/main.js
+++ server/main.js
@@ -125,8 +125,6 @@ streamRouter.use(function(req, res, next) {
 
 // initialize server to listen on specified port
 //var server = expressApp.listen(config.webPort);
-//var tlsServer = https.createServer(tlsOptions, expressApp).listen(config.tlsPort);
-
 //http.createServer(greenlock.middleware(redir)).listen(config.webPort);
 var tlsServer = https.createServer(tlsOptions, expressApp).listen(config.tlsPort);
 
@@ -151,60 +149,19 @@ io.on('disconnect', function(socket) {
 	console.log('a user disconnected');
 });
 
-/*
-	code from main.js of electron-quick-start
-*/
-
-/*const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+/*const {app, BrowserWindow} = require('electron')
 
 function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1300, height: 800})
 
-  // and load the index.html of the app.
-  mainWindow.loadURL('http://localhost:8080')
+	// Create the browser window.
+	win = new BrowserWindow({width: 1200, height: 800})
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+	// and load the index.html of the app.
+	//win.loadFile('public/index.html')
+	win.loadURL('https://localhost:8443')
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-	// Dereference the window object, usually you would store windows
-	// in an array if your app supports multi windows, this is the time
-	// when you should delete the corresponding element.
-	mainWindow = null
-  })
+	win.webContents.openDevTools()
+
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-	app.quit()
-  }
-})
-
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-	createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.*/
+app.on('ready', createWindow)*/
